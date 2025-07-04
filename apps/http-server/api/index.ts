b73777b657696786e99249cdfecc25a6922d6a83
aman-deep940 <amandeep.vdoit@gmail.com>
2025-07-04 17:51:29 +0530

    dsdafsd

--- apps/http-server/api/index.ts
+++ apps/http-server/api/index.ts
@@ -26,6 +26,4 @@ app.use("/api", chatRouter);
 app.use("/api", roomRouter);
 app.use("/api", userRouter);
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
 module.exports = app;
